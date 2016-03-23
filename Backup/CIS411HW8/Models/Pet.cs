b4bc5bf65aa2e623f82bbc964304c91d343362b6
Mike Crabtree <mike.d.crabtree@gmail.com>
2016-03-22 20:39:52 -0400

    Trying to fix
    trying to fix an error with the deployment, changed .gitignore to
    exactly what appears in the MS example

---
+++ Backup/CIS411HW8/Models/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace CIS411HW8.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime DateReceived { get; set; }
+
+    }
+}
